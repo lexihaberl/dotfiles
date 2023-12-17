39b96aa29fd5ea8ed92c9c02b240dc5fb3833fcc
Alexander Haberl <azteke42@gmail.com>
2023-12-17 12:41:35 +0100

    add Cargo/Conda for fish shell

--- fish/config.fish
+++ fish/config.fish
@@ -1,3 +1,18 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
+    fish_add_path $HOME/.cargo/bin
 end
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+if test -f /home/lexi/miniconda3/bin/conda
+    eval /home/lexi/miniconda3/bin/conda "shell.fish" "hook" $argv | source
+else
+    if test -f "/home/lexi/miniconda3/etc/fish/conf.d/conda.fish"
+        . "/home/lexi/miniconda3/etc/fish/conf.d/conda.fish"
+    else
+        set -x PATH "/home/lexi/miniconda3/bin" $PATH
+    end
+end
+# <<< conda initialize <<<
+
