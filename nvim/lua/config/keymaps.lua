982bae496688d30f9bd8ec4f51ecaf8361d327de
Alexander Haberl <azteke42@gmail.com>
2024-02-11 20:13:40 +0100

    Change keymaps to have better window switching on corne

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,3 +1,7 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+vim.keymap.set("n", "<leader>wn", "<C-w>h", { desc = "Go to left window", remap = true })
+vim.keymap.set("n", "<leader>we", "<C-w>j", { desc = "Go to lower window", remap = true })
+vim.keymap.set("n", "<leader>wi", "<C-w>k", { desc = "Go to upper window", remap = true })
+vim.keymap.set("n", "<leader>wo", "<C-w>l", { desc = "Go to right window", remap = true })
